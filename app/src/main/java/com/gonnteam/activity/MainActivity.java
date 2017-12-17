935df0cfced161915e3ef7f6c571b0a6c70712ff
Huynh Thien <thienromtn55@gmail.com>
2017-12-17 17:12:19 +0700

    first commit

---
+++ app/src/main/java/com/gonnteam/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gonnteam.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gonnteam.smartlamp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
