d97556b7b6aee4309bab2bb5ed1b960676e9ba16
Huynh Thien <thienromtn55@gmail.com>
2017-12-17 21:31:41 +0700

    add menu item

---
+++ app/src/main/java/com/gonnteam/fragment/ControlsFragment.java
@@ -0,0 +1,30 @@
+package com.gonnteam.fragment;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gonnteam.smartlamp.R;
+
+/**
+ * Created by MrThien on 2017-12-17.
+ */
+
+public class ControlsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.controls_fragment, container, false);
+
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Bảng điều khiển");
+    }
+}
